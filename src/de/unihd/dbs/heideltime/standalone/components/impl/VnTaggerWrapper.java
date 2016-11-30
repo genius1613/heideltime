218cdce7bcc0fdded0a1882c16268858edb3a576
genius1613 <tienbm90@gmail.com>
2016-12-01 00:43:24 +0700

    first commit

---
+++ src/de/unihd/dbs/heideltime/standalone/components/impl/VnTaggerWrapper.java
@@ -0,0 +1,15 @@
+package de.unihd.dbs.heideltime.standalone.components.impl;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+/**
+ * Created by tienbm90 on 26/11/2016.
+ */
+public class VnTaggerWrapper extends JCasAnnotator_ImplBase {
+    @Override
+    public void process(JCas jCas) throws AnalysisEngineProcessException {
+
+    }
+}
